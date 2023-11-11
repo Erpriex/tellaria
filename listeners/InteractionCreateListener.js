03f7a0fd723e9925a3c2d15784be95c5f834eb93
Erpriex <erpriex@gmail.com>
2023-11-11 17:20:24 +0100

    🚧 Thread creation

--- listeners/InteractionCreateListener.js
+++ listeners/InteractionCreateListener.js
@@ -1,3 +1,5 @@
+const { ButtonInteraction, ChatInputCommandInteraction, ChannelType } = require("discord.js");
+
 module.exports = class MessageCreateListener {
 
     constructor(main) {
@@ -7,8 +9,28 @@ module.exports = class MessageCreateListener {
     onInteractionCreate() {
         this.main.bot.on('interactionCreate', async interaction => {
             console.log(interaction);
-            if(this.main.commandTellaria.match(interaction.commandName)){
-                this.main.commandTellaria.action(interaction);
+
+            if(interaction instanceof ChatInputCommandInteraction){
+                if(this.main.commandTellaria.match(interaction.commandName)){
+                    this.main.commandTellaria.action(interaction);
+                }
+            }
+
+            if(interaction instanceof ButtonInteraction){
+                console.log("Button interaction");
+                interaction.deferUpdate();
+                const thread = await interaction.channel.threads.create({
+                    name: 'tellaria-' + interaction.user.username,
+                    autoArchiveDuration: 60,
+                    type: ChannelType.PrivateThread,
+                    reason: 'Discussion Tellaria',
+                });
+
+                await thread.members.add(interaction.user.id);
+
+                /*await interaction.reply("C'est parti !").then((msg) => {
+                    console.log("Réponse envoyée");
+                })*/
             }
         });
     }
